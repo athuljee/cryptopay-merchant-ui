e9d65702362a703fd253c3c9d96ea721a0fa660e
Amal Jeev <amaljeev@interlandtech.com>
2026-03-15 15:48:57 +0530

    changes

--- lib/screens/transaction_history.dart
+++ lib/screens/transaction_history.dart
@@ -489,7 +489,7 @@ class _TransactionHistoryState extends State<TransactionHistory> {
                             ),
                           ),
                         ],
-                      ),
+                      )
       ),
     );
   }
